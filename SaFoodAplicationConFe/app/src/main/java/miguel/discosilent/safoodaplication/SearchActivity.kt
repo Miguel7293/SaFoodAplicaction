a4a5570f1a6fce8686ee0650c7f2778316ccf2ca
jukii4 <114840565+jukii4@users.noreply.github.com>
2025-01-14 20:12:11 -0500

    cambios en search

--- SaFoodAplicationConFe/app/src/main/java/miguel/discosilent/safoodaplication/SearchActivity.kt
+++ SaFoodAplicationConFe/app/src/main/java/miguel/discosilent/safoodaplication/SearchActivity.kt
@@ -1,25 +1,35 @@
 package miguel.discosilent.safoodaplication
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.widget.addTextChangedListener
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.textfield.TextInputEditText
 
 class SearchActivity : AppCompatActivity() {
 
-    val restaurant = RestaurantProvider.restaurants
+    private val restaurant = RestaurantProvider.restaurants
+    private lateinit var textSearch: TextInputEditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_search)
+
+        textSearch = findViewById(R.id.SearchText)
+
+
+        textSearch.addTextChangedListener { reference ->
+            val restaurantFiltered = restaurant.filter { it.name.contains(reference.toString()) }
+        }
+
         initRecycler()
     }
 
-    fun initRecycler(){
+    private fun initRecycler() {
         val recyclerView = findViewById<RecyclerView>(R.id.list_searched)
         recyclerView.layoutManager = LinearLayoutManager(this)
         val adapter = RestAdapter(restaurant)
